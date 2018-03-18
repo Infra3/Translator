cfb7aea4fe56c69ac05dfd7539ad21d6938bd567
Infra3 <37303686+Infra3@users.noreply.github.com>
2018-03-18 11:41:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+
+'use strict';
+const translate = require('google-translate-api');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const restService = express();
+
+
+
+restService.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+
+restService.use(bodyParser.json());
+
+restService.post('/weatherinfo', (req, res) => {
+    // Get the city and date from the request
+    let any1 = req.body.result.parameters['any']; // city is a required param
+    // Get the date for the weather forecast (if present)
+   
+    // Call the weather API
+   translate(any1, {from: 'en', to: 'nl'}).then(res => {
+ 
+	
+	  res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ 'speech': res.text, 'displayText': res.text}));
+     
+}).catch(err => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
+}); 
+
+
+});
+
+
+restService.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: ' + (process.env.PORT || 8000));
+});
